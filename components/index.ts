599877f7141bc0b0d0db6db69dc4664c4bc05d72
Parvana Guliyeva <parvanaguliyeva@006871-MAC.local>
2023-12-09 21:25:56 +0400

    first commit

---
+++ components/index.ts
@@ -0,0 +1,24 @@
+"use client";
+import Hero from "./Hero";
+import CustomButon from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButon,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
